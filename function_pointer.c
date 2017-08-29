505cf75eaeb38d8244d7742aaf4f0a6dc6c5eee2
Milller.Jo <jsjmcch@gmail.com>
2017-08-29 19:08:35 +0900

    Update function_pointer.c

--- function_pointer.c
+++ function_pointer.c
@@ -371,7 +371,75 @@ $ ./function_pointer7.exe
 7
 BBB
 =================================================================================
+#include <stdio.h>
 
+struct Person {
+    char name[30];
+    int age;
+    void (*print)(struct Person *);
+};
+
+void print(struct Person *p)
+{
+    printf("%s %d\n", p->name, p->age);
+}
+
+void executer(void (*fp[])(struct Person *), struct Person p[], int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        fp[i](&p[i]);
+    }
+}
+
+#if 0
+typedef void (*fp) (struct Person *);
+
+fp getPrintFunc( struct Person *ps ) {
+
+    return ps->print;
+}
+#else
+void (*getPrintFunc()) (struct Person *)
+{
+    return print;
+}
+#endif
+
+
+int main()
+{
+    struct Person p[3];
+    p[0].print = print;
+    p[1].print = print;
+    p[2].print = print;
+
+    scanf("%s %d %s %d %s %d",
+        p[0].name,& p[0].age,
+        p[1].name,& p[1].age,
+        p[2].name,& p[2].age
+    );
+
+    void (*fp[3])(struct Person *);
+
+    for (int i = 0; i < sizeof(p) / sizeof(struct Person); i++)
+    {
+        fp[i] = getPrintFunc(&p[i]);
+    }
+
+    executer(fp, p, sizeof(p) / sizeof(struct Person));
+
+    return 0;
+}
+/*
+입력
+홍대용 49 정약용 18 박제가 30
+
+결과
+홍대용 49
+정약용 18
+박제가 30
+*/
 =================================================================================
 
 =================================================================================
